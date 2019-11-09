d7cbf7240f2a76a8c5ea31daa719bdb20b089b12
cinxdy <cinxdys2@gmail.com>
2019-11-09 12:59:47 +0900

    NNF

--- tree.c
+++ tree.c
@@ -27,6 +27,8 @@ struct Tree* newTree(int sign, int prop);
 struct Tree* parseToTree(struct Tree* t);
 //int checkSyntax();
 void printTree(struct Tree* t);
+struct Tree* not(struct Tree* t);
+struct Tree* NNF(struct Tree* t);
 
 #if 1
 int main(){
@@ -36,11 +38,20 @@ int main(){
     if(root == NULL)
         printf("error!\n");
 
+    printf("read == ");
+    printTree(root);
+    printf("\n");
+
+    root = NNF(root);
+    printf("NNF == ");
+    printTree(root);
+    printf("\n\n\n");
+
     DPrintf(printf("root=%p \n",root););
 
     //DPrintf(printf("root=%p, root->left=%p, root->right=%p \n",root,root->left,root->right););
 
-    printTree(root);
+    
 }
 #endif
 
@@ -95,6 +106,7 @@ struct Tree* parseToTree(struct Tree* t){ // close the bracket or not  true: clo
     
     return t;
 }
+
 /*
 int checkSyntax(){
     DPrintf(printf("< checkSyntax\n"));
@@ -126,18 +138,13 @@ void printTree(struct Tree* t){
     if(t == NULL) return ;
     DPrintf(printf("< printTree tree=%p, tree->left=%p, tree->right=%p\n", t, t->left, t->right););
 
-    
-
-    if(t->sign == 0)
-        printf(" a%d", t->prop);
-    else{
-        if(t->sign == 1)
-            printf(" (and");
-        else if(t->sign == 2)
-            printf(" (or");
-        else if(t->sign == 3)
-            printf(" (not");
+    switch(t->sign){
+        case 0: printf(" a%d", t->prop); break;
+        case 1: printf(" (and"); break;
+        case 2: printf(" (or");  break;
+        case 3: printf(" (not"); break;
     }
+
 
     printTree(t->left);
     printTree(t->right);
@@ -146,4 +153,39 @@ void printTree(struct Tree* t){
         printf(")");
 
     DPrintf(printf("> printTree tree=%p\n",t););
+}
+
+struct Tree* not(struct Tree* t){
+    DPrintf(printf("< not tree=%p\n",t););
+
+    if(t->sign == 0) t->prop *= -1;
+    else if(t->sign == 1 || t->sign == 2) {
+        t->sign = 3- t->sign;
+        t->left = not(t->left);
+        t->right = not(t->right);
+    }
+    else if(t->sign == 3) t = t->left;
+    
+    return t;
+    DPrintf(printf("> not tree=%p\n",t););
+}
+
+struct Tree* NNF(struct Tree* t){
+    DPrintf(printf("< NNF tree=%p\n",t););
+
+    if(t == NULL){
+        //printf("error! nullptr!\n");
+        return t;
+    }
+
+    if(t->sign == 3){ // meet 'not'
+        t = not(t->left);
+    }
+    else if(t->sign==1 || t->sign==2){ // and || or
+        t->left = NNF(t->left);
+        t->right = NNF(t->right);
+    }
+
+    DPrintf(printf("> NNF tree=%p\n",t););
+    return t;
 }
